fcd415c618084db9d48f9b82a6a3fd7d8d5db578
Sean Perkins <seanperkins@Seans-MBP.hsd1.in.comcast.net>
2020-11-15 10:59:37 -0500

    first commit

---
+++ src/Components/Site/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom';
+import Home from './Home';
+import Resources from './Resources';
+
+const Sidebar = () => {
+    return (
+        <div className="sideBar">
+            
+        </div>
+    )
+}
